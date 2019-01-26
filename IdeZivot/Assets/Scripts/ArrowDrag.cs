cd962df7a4366dc1819a5cf53efb49d49b75a09f
scoutsenpai <filipgolaboski@gmail.com>
2019-01-26 23:14:33 +0100

    Created clock mechanism

---
+++ IdeZivot/Assets/Scripts/ArrowDrag.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[System.Serializable]
+public class HandlePosition : UnityEvent<float> { }
+
+public class ArrowDrag : MonoBehaviour, IPointerDownHandler
+{
+    bool act = false;
+    public HandlePosition handlePosition;
+
+    private void Update()
+    {
+        if (act)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                transform.LookAt(mouse, Vector3.forward);
+                Vector3 eulers = transform.rotation.eulerAngles;
+                eulers.z = 0;
+                transform.Rotate(eulers);
+            }
+
+            if(Input.GetMouseButtonUp(0))
+            {
+                handlePosition.Invoke(transform.rotation.eulerAngles.z);
+                act = false;
+            }
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        act = true;
+    }
+}
---
+++ IdeZivot/Assets/Scripts/ClockController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockController : MonoBehaviour
+{
+    public float bigHandleFinal;
+    public float smallHandleFinal;
+    bool smallHandleFinish = false;
+    bool bigHandleFinish = false;
+    public void BigHandleRotation(float rotation)
+    {
+        float twelvePart = rotation / 30;
+        float diff = bigHandleFinal - twelvePart;
+        if (diff < 0.1f || diff > -0.1f) {
+            if (smallHandleFinish)
+            {
+                PickableObject p = GetComponentInChildren<PickableObject>();
+                if (p != null) {
+                    p.PickItem();
+                }
+            }
+            else
+            {
+                bigHandleFinish = true;
+            }
+        }
+    }
+
+    public void SmallHandleRotation(float rotation)
+    {
+        float twelvePart = rotation / 30;
+        float diff = bigHandleFinal - twelvePart;
+        if (diff < 0.1f || diff > -0.1f)
+        {
+            if (bigHandleFinish)
+            {
+                PickableObject p = GetComponentInChildren<PickableObject>();
+                if (p != null) { p.PickItem(); }
+            }
+            else
+            {
+                smallHandleFinish = true;
+            }
+        }
+    }
+
+}
