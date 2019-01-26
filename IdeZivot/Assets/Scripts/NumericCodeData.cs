13c69b89a5b6f9e9a5ff7bdc81185022db388ded
scoutsenpai <filipgolaboski@gmail.com>
2019-01-26 14:50:56 +0100

    Created NumericCode

---
+++ IdeZivot/Assets/Scripts/NumericCodeData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumericCodeData : MonoBehaviour
+{
+    public int[] codeArray;
+
+    public bool CheckCode(int[] code)
+    {
+        if(code.Length != codeArray.Length)
+        {
+            return false;
+        }
+
+        for(int i=0;i<code.Length; i++)
+        {
+            if(code[i] != codeArray[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ IdeZivot/Assets/Scripts/NumericCodeSystem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NumericCodeSystem : MonoBehaviour
+{
+    public NumericCodeData numericCodeData;
+    int[] inputArray;
+
+    public UnityEvent onCodeSuccess;
+    public UnityEvent onCodeFail;
+
+    private void Awake()
+    {
+        inputArray = new int[numericCodeData.codeArray.Length];
+    }
+
+
+    public void CheckCode()
+    {
+        if (numericCodeData.CheckCode(inputArray))
+        {
+            onCodeSuccess.Invoke();
+        }
+        else
+        {
+            onCodeFail.Invoke();
+        }
+    }
+
+    public void SetCode(int[] newInputArray)
+    {
+        if(newInputArray.Length == inputArray.Length)
+        {
+            inputArray = newInputArray;
+        }
+    }
+
+    public void SetCodeAt(int val, int position)
+    {
+        if(position >= inputArray.Length)
+        {
+            inputArray[position] = val;
+        }
+    }
+
+}
