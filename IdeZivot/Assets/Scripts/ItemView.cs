a827ac532eb62f79aaabaad368ee51342644ef05
AngelovskiN <nikola.angelovski@webfactory.mk>
2019-01-26 22:31:11 +0100

    Created ItemView

---
+++ IdeZivot/Assets/Scripts/ItemView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemView : MonoBehaviour
+{
+    public Image itemSprite;
+    public Button selectionCtrl;
+
+    public void SetView(Sprite sprite, Action selectAction)
+    {
+        itemSprite.sprite = sprite;
+        itemSprite.enabled = true;
+        selectionCtrl.onClick.AddListener(selectAction.Invoke);
+    }
+}
