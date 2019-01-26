f6d77115771cf3f9f4c233a305915e4dbd45beb5
scoutsenpai <filipgolaboski@gmail.com>
2019-01-26 14:50:46 +0100

    Created InventoryData

---
+++ IdeZivot/Assets/Scripts/InventoryData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "InventoryData")]
+public class InventoryData : ScriptableObject
+{
+    public InventoryItem currentSelectedItem;
+    public InventoryItem[] items;
+}
---
+++ IdeZivot/Assets/Scripts/UsePlace.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class UsePlace : MonoBehaviour
+{
+    public InventoryItem neededItem;
+    public InventoryData inventoryData;
+    public UnityEvent onItemSuccess;
+    public UnityEvent onItemFail;
+
+    public void UseItem()
+    {
+        if (inventoryData.currentSelectedItem.name.Equals(neededItem.name) && inventoryData.currentSelectedItem.canBeUsed)
+        {
+            Debug.Log("Item:" + inventoryData.name);
+            onItemSuccess.Invoke();
+        }
+        else
+        {
+            Debug.Log("Item Failed:" + inventoryData.name);
+            onItemFail.Invoke();
+        }
+    }
+}
