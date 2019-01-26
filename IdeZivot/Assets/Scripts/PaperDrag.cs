af15b7d52671002b520b4a686c2981fb68cadcb7
scoutsenpai <filipgolaboski@gmail.com>
2019-01-27 00:48:11 +0100

    Created PaperDrag

---
+++ IdeZivot/Assets/Scripts/PaperDrag.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PaperDrag : MonoBehaviour, IBeginDragHandler, IDragHandler
+{
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Vector3 newPos = Camera.main.ScreenToWorldPoint(eventData.position);
+        newPos.z = 0;
+        transform.position = newPos;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 newPos = Camera.main.ScreenToWorldPoint(eventData.position);
+        newPos.z = 0;
+        transform.position = newPos;
+
+    }
+
+}
