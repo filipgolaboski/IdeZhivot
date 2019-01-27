94bb08de8af9e131c87e3d1a091e735c08b60b42
scoutsenpai <filipgolaboski@gmail.com>
2019-01-27 05:03:43 +0100

    Created Computer

---
+++ IdeZivot/Assets/Scripts/PCInputChecker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PCInputChecker : MonoBehaviour
+{
+    string inputString;
+    public string compareString;
+    public PickableObject pickableObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if(inputString.Equals(compareString))
+            {
+                pickableObject.PickItem();
+            }
+        }
+    }
+
+    public void OnTextChanged(string val)
+    {
+        inputString = val;
+    }
+}
