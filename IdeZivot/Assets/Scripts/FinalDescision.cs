3eb4b0037eb522952569f0a81e3df3df8a9479a9
AngelovskiN <nikola.angelovski@webfactory.mk>
2019-01-27 17:08:21 +0100

    Created FinalDesicitiomn

---
+++ IdeZivot/Assets/Scripts/FinalDescision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalDescision : MonoBehaviour
+{
+    public Button vmro;
+    public Button sdsm;
+    public Button eu;
+
+    public void PickVmro()
+    {
+        sdsm.gameObject.SetActive(false);
+        eu.gameObject.SetActive(false);
+    }
+
+    public void PickSdsm()
+    {
+        vmro.gameObject.SetActive(false);
+        eu.gameObject.SetActive(false);
+    }
+
+    public void PickEu()
+    {
+        sdsm.gameObject.SetActive(false);
+        vmro.gameObject.SetActive(false);
+    }
+}
