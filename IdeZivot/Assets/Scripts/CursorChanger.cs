548df1ea5bb4f54efb49c976a3ab5f5ffa7d1830
AngelovskiN <nikola.angelovski@webfactory.mk>
2019-02-13 23:08:49 +0100

    On cursor hover behaviour

---
+++ IdeZivot/Assets/Scripts/CursorChanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CursorChanger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Texture2D hoverTexture;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Cursor.SetCursor(hoverTexture, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void OnDestroy()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
